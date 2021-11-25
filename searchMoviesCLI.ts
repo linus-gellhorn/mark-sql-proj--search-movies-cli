533e424cc88301b500b0ec62d9d9f50c4e12c615
Linus Gellhorn <linus.g@hotmail.com>
2021-11-25 21:00:45 +0000

    Initial query to test access of database

--- searchMoviesCLI.ts
+++ searchMoviesCLI.ts
@@ -4,5 +4,14 @@ import { Client } from "pg";
 //As your database is on your local machine, with default port,
 //and default username and password,
 //we only need to specify the (non-default) database name.
-const client = new Client({ database: 'omdb' });
-console.log("Welcome to search-movies-cli!");
+
+async function searchMovies() {
+  const client = new Client({ database: "omdb" });
+  console.log("Welcome to search-movies-cli!");
+  await client.connect();
+  const res = await client.query("SELECT name from movies order by id limit 5");
+  console.table(res.rows);
+  await client.end();
+}
+
+searchMovies();
